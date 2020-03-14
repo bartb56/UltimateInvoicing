97d057472277333837c2c5ee3f72be34cef8da75
bartb56 <bartblokhuis123@outlook.com>
2020-03-14 22:56:13 +0100

    Removed test variable

--- src/UltimateInvoicing.Web/Startup.cs
+++ src/UltimateInvoicing.Web/Startup.cs
@@ -26,11 +26,9 @@ namespace UltimateInvoicing.Web
         {
             services.AddRazorPages();
 
-            var conString = Configuration.GetConnectionString("DefaultConnection");
             services.AddDbContext<UltimateInvoicingContext>(options =>
-            {
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
-            });
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
